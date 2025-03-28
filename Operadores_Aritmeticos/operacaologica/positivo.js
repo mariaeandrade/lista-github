acc6359303dcff45298f4ec2a61f6951fc732072
Andrade <dudadeandrade13@gmail.com>
2025-03-28 11:50:56 -0300

    feat(operadores-aritmeticos)

--- Operadores_Aritmeticos/operacaologica/positivo.js
+++ Operadores_Aritmeticos/operacaologica/positivo.js
@@ -1,8 +1,12 @@
-let num1 = 10;
-let num2 = (2);
-
-if (num1 && num2 > 0)
-console.log ("Os dois são positivos.")
-else {
-    console.log ("Os dois não são positivos.")
+let num1 = 12;
+let num2 = 9;
+switch ("true"){
+    case (num1 && num2 > 0 ):
+    console.log ("Os dois são positivos");
+    break;
+    case (num1 > 0 && num2 < 0):
+    console.log ("Apenas um é positivo");
+    break;
+    default:
+        console.log ("Os dois são negativos")
 }
